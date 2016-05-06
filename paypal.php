cb5ddb02fb4355c723902e2958bd431aa3c162dc
hugopiso@gmail.com <hugopiso@gmail.com>
2016-05-06 18:10:23 +0200

    get access token

--- paypal.php
+++ paypal.php
@@ -1,9 +1,8 @@
 <?php
 
-/**
- *
- */
-class paypal
+require_once 'config.php';
+
+class Paypal
 {
 
     function __construct()
@@ -13,6 +12,26 @@ class paypal
 
     public function getAccessToken()
     {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, "https://api.sandbox.paypal.com/v1/oauth2/token");
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERPWD, CLIENT_ID.":".CLIENT_SECRET);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "grant_type=client_credentials");
+
+        $result = curl_exec($ch);
+
+        if (empty($result)) {
+            die("Error: No response.");
+        } else {
+            $json = json_decode($result);
+            return $json->access_token;
+        }
+
+        curl_close($ch);
 
     }
 
@@ -25,5 +44,6 @@ class paypal
     {
         
     }
-
 }
+
+
